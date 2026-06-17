48f21a4d0418820501555b98ba5fdc35134d8c00
choyounga1983 <choyounga1983@gmail.com>
2026-06-17 15:35:09 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -76,6 +76,7 @@ function buildChrome(active){
           ${links.map(k=>`<a href="${pages[k]}" data-i18n="nav.${k}"></a>`).join("")}</div>
         <div class="foot-col foot-contact"><h5 data-i18n="foot.contact"></h5>
           <div class="fc"><span class="fc-name">조영아 · Strella Young</span><span class="fc-role">영웨이브 본사 대표</span><a href="mailto:youngwavecreative@gmail.com">youngwavecreative@gmail.com</a></div>
+          <div class="fc"><span class="fc-name">Duong Thi Huyen Tran</span><span class="fc-role">영웨이브 베트남 공동대표</span></div>
           <div class="fc"><span class="fc-name">Brooks Kim · 김현우</span><span class="fc-role">한국 지사 · Brooks Global 대표</span><a href="mailto:brooksservices.kim@gmail.com">brooksservices.kim@gmail.com</a></div>
           <div class="fc"><span class="fc-name">Jenny Go · 고은주</span><span class="fc-role">한국 지사 · Brooks Global Sales Director</span><a href="mailto:jinniko7490@naver.com">jinniko7490@naver.com</a></div>
           <a class="fc-addr">HCMC · Vincom Center, District 1 &nbsp;/&nbsp; Incheon · Brooks Global (Korea)</a></div>
